62c62fa7ea8a40616d95af40066fe8d0e5d1562f
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-14 23:11:14 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -20,7 +20,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return math.degrees(angle_rads), math, 0.0
+    return math.degrees(angle_rads), 0.0, 0.0
 
 
 def to_celsius(temperature: float) -> float:
