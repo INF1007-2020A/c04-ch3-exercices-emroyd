509debf7a350a0e23831080469941691ec91864e
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-14 22:31:56 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,7 +5,7 @@
 import math
 #Calculer la racine carré
 def square_root(a: float) -> float:
-nombre = input('Veuillez rentrer un nombre:')
+    nombre = input('Veuillez rentrer un nombre:')
 print('La racine carrée de', nombre, 'est', square_root(nombre))
     return 0.0
 
