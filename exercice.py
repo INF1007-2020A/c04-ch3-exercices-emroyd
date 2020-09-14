851d615b0873af321fac66ba6110a1d15285978a
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-14 22:36:08 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -11,7 +11,7 @@ print('La racine carrée de', nombre, 'est', square_root(nombre))
 
 #Nombre au carré
 nombre=input('Veuillez rentrer un nombre:')
-print('Le carré de', nombre, 'est', )
+print('Le carré de', nombre, 'est', nombre**2)
     return 0.0
 
 
