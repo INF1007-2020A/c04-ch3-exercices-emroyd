3eb3bf93d667d54ee95d1c3222a0f37e73dbb25b
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-14 22:49:20 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,8 +13,7 @@ def square(a: float) -> float:
 
 
 def average(a: float, b: float, c: float) -> float:
-    avg
-    return ((a+b+c)/len(a+b+c))
+    return ((a+b+c)/3)
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     return 0.0
