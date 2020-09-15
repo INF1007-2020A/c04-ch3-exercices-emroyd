27067e7b6ae7994ce16b2b4baf4c6ae938d33f74
emroyd <emmanuelle.roy-dubreuil@polymtl.ca>
2020-09-15 00:08:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,7 +31,7 @@ def to_celsius(temperature: float) -> float:
 
 
 def to_farenheit(temperature: float) -> float:
-    return ((temperature+32)*9/5)
+    return ((temperature*9/5)*9/5)
 
 
 def main() -> None:
